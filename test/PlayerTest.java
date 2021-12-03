f674c2f685eb4865a656dbcae58d46c0415b01a3
g.salvio <giuseppe.salvio@finconsgroup.com>
2021-12-03 12:20:44 +0100

    refactor name

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -4,14 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerTest {
     @Test
-    void creo_un_player_che_inizia_con_score_a_0() {
+    void create_a_player_that_start_with_score_to_0() {
         Player player = new Player();
 
         assertEquals(Score.ZERO, player.getScore());
     }
 
     @Test
-    void creo_un_player_e_modifico_lo_score_in_LOVE() {
+    void create_a_player_that_start_with_score_to_LOVE() {
         Player player = new Player();
         player.setScrore(Score.LOVE);
 
