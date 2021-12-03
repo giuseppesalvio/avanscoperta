35e1fb4a46a2a3809c7060aedcc9016e997985bf
g.salvio <giuseppe.salvio@finconsgroup.com>
2021-12-03 11:18:45 +0100

    get e set di score di player

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,11 @@
 public class Player {
+    private String score="0";
+
     public String getScore() {
-        return "0";
+        return score;
+    }
+
+    public void setScrore(String score) {
+        this.score = score;
     }
 }
--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -4,10 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerTest {
     @Test
-    void player_ha_uno_score() {
+    void init_player_ha_uno_score_a_0() {
         Player player = new Player();
 
         assertThat(player.getScore()).isEqualTo("0");
     }
+
+    @Test
+    void modifica_lo_score_di_player_con_LOVE() {
+        Player player = new Player();
+        player.setScrore("LOVE");
+
+        assertThat(player.getScore()).isEqualTo("LOVE");
+    }
+
 
 }
